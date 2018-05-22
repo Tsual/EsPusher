9f9fb170ce6432de313fc09eeda8d98846450934
张良 <562974055@qq.com>
2018-05-22 21:48:34 +0800

    Initial commit

---
+++ app/src/main/java/org/easydarwin/util/LoginUser.java
@@ -0,0 +1,31 @@
+package org.easydarwin.util;
+
+import java.util.Date;
+
+public class LoginUser {
+    private static LoginUser Current;
+
+    public static LoginUser getCurrentUser() {
+        if (Current == null)
+            Current = new LoginUser();
+        return Current;
+    }
+
+    public static void Reset() {
+        Current = null;
+    }
+
+    private LoginUser() {
+
+    }
+
+
+    public boolean isLogin = false;
+    public String LID = "";
+    public int UID = -1;
+    public String ORI_PWD = "";
+    public String Name = "";
+    public boolean hasFacedata = false;
+    public String LoginToken = "";
+    public Date LoginDate = null;
+}
